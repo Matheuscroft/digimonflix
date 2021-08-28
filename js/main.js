ad6d5ceb5e69875c149d064da5f6ef246ac7961b
Matheuscroft <matheus.sdoliveira@gmail.com>
2021-08-28 20:40:05 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function verTrailer() {
+    window.open('https://www.youtube.com/watch?v=CCtHYdbOgcs')
+}
+
+function verInfo() {
+    window.open('https://www.themoviedb.org/tv/98034?language=pt-BR')
+}
+
